4749d42ff0308ff594bcded30b51c32d7b1d5b0d
beatrizcamilo <abeatrizcamilo@gmail.com>
2025-02-04 17:58:27 -0300

    Classe de serviços: operações de buscas

--- Desktop/webservice/src/main/java/br/com/webservice/control/FuncionarioControle.java
+++ Desktop/webservice/src/main/java/br/com/webservice/control/FuncionarioControle.java
@@ -2,18 +2,31 @@ package br.com.webservice.control;
 
 import br.com.webservice.model.Funcionario;
 
+import br.com.webservice.service.FuncionarioServico;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("/funcionarios")
 public class FuncionarioControle{
 
+    @Autowired
+    private FuncionarioServico servico;
+
     @GetMapping()
-    public ResponseEntity<Funcionario> getFuncionarios(){
-        Funcionario func1 = new Funcionario(null,"Dolores Cabral","dolorescabral@gmail.com");
-        return ResponseEntity.ok().body(func1);
+    public ResponseEntity<List<Funcionario>> getFuncionarios(){
+        List<Funcionario> listaFuncionarios = servico.listar();
+        return ResponseEntity.ok().body(listaFuncionarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Funcionario> getFuncionario(@PathVariable Long id){
+        return ResponseEntity.ok().body(servico.encontrarPorId(id));
     }
 }
--- Desktop/webservice/src/main/java/br/com/webservice/service/FuncionarioServico.java
+++ Desktop/webservice/src/main/java/br/com/webservice/service/FuncionarioServico.java
@@ -1,4 +1,26 @@
 package br.com.webservice.service;
 
+import br.com.webservice.model.Funcionario;
+import br.com.webservice.repository.FuncionarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FuncionarioServico {
+
+    @Autowired
+    private FuncionarioRepositorio repositorio;
+
+    public List<Funcionario> listar(){
+        return repositorio.findAll();
+    }
+
+    public Funcionario encontrarPorId(Long id){
+        Optional<Funcionario> funcionarioEncontrado = repositorio.findById(id);
+        return funcionarioEncontrado.get();
+    }
+
 }
